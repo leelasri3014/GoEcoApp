3066be23041c810e12ef5b18c29c4b08e31a424a
leelasri3014 <128228058+leelasri3014@users.noreply.github.com>
2024-05-12 19:53:47 +0530

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,230 @@
+import React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Switch,
+} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+const SECTIONS = [
+  {
+    header: 'Preferences',
+    icon: 'settings',
+    items: [
+      { icon: 'user', color: '#fe9400', label: 'Edit Profile', type: 'link' },
+      {
+        icon: 'lock',
+        color: '#007afe',
+        label: 'Change Password',
+        value: false,
+        type: 'link',
+      },
+      {
+        icon: 'bell',
+        color: '#007afe',
+        label: 'Push Notifications',
+        value: true,
+        type: 'boolean',
+      },
+      { icon: 'navigation', color: '#32c759', label: 'Location', value:'true', type: 'boolean' },
+      {
+        icon: '',
+        color: '#007afe',
+        label: 'Edit Map Location',
+        value: true,
+        type: 'link',
+      },
+    ],
+  },
+  {
+    header: 'Help',
+    icon: 'help-circle',
+    items: [
+      { icon: 'flag', color: '#8e8d91', label: 'Report Bug', type: 'link' },
+      { icon: 'mail', color: '#007afe', label: 'Contact Us', type: 'link' },
+    ],
+  },
+  {
+    header: 'Content',
+    icon: 'align-center',
+    items: [
+      { icon: 'save', color: '#32c759', label: 'Saved', type: 'link' },
+      { icon: 'download', color: '#fd2d54', label: 'Downloads', type: 'link' },
+    ],
+  },
+];
+
+export default function Settings() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.profile}>
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}>
+            <View style={styles.profileAvatarWrapper}>
+              <Image
+                alt=""
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80',
+                }}
+                style={styles.profileAvatar} />
+
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}>
+                <View style={styles.profileAction}>
+                  <FeatherIcon
+                    color="#fff"
+                    name="edit-3"
+                    size={15} />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+
+          <View>
+            <Text style={styles.profileName}>Sravanthi</Text>
+
+            <Text style={styles.profileAddress}>
+              abcdefghijjkhjfffjhjhll;;ljffgfgggfgfgfgfgf
+            </Text>
+          </View>
+        </View>
+
+        {SECTIONS.map(({ header, items }) => (
+          <View style={styles.section} key={header}>
+            <Text style={styles.sectionHeader}>{header}</Text>
+            {items.map(({ label, icon, type, value, color }, index) => {
+              return (
+                <TouchableOpacity
+                  key={label}
+                  onPress={() => {
+                    // handle onPress
+                  }}>
+                  <View style={styles.row}>
+                    <View style={[styles.rowIcon, { backgroundColor: color }]}>
+                      <FeatherIcon
+                        color="#fff"
+                        name={icon}
+                        size={18} />
+                    </View>
+
+                    <Text style={styles.rowLabel}>{label}</Text>
+
+                    <View style={styles.rowSpacer} />
+
+                    {type === 'boolean' && <Switch value={value} />}
+
+                    {type === 'link' && (
+                      <FeatherIcon
+                        color="#0c0c0c"
+                        name="chevron-right"
+                        size={22} />
+                    )}
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 24,
+  },
+  /** Profile */
+  profile: {
+    padding: 24,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileAvatarWrapper: {
+    position: 'relative',
+  },
+  profileAvatar: {
+    width: 72,
+    height: 72,
+    borderRadius: 9999,
+  },
+  profileAction: {
+    position: 'absolute',
+    right: -4,
+    bottom: -10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 28,
+    height: 28,
+    borderRadius: 9999,
+    backgroundColor: '#007bff',
+  },
+  profileName: {
+    marginTop: 20,
+    fontSize: 19,
+    fontWeight: '600',
+    color: '#414d63',
+    textAlign: 'center',
+  },
+  profileAddress: {
+    marginTop: 5,
+    fontSize: 16,
+    color: '#989898',
+    textAlign: 'center',
+  },
+  /** Section */
+  section: {
+    paddingHorizontal: 24,
+  },
+  sectionHeader: {
+    paddingVertical: 12,
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#9e9e9e',
+    textTransform: 'uppercase',
+    letterSpacing: 1.1,
+  },
+  /** Row */
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 50,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+  rowIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: 9999,
+    marginRight: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rowLabel: {
+    fontSize: 17,
+    fontWeight: '400',
+    color: '#0c0c0c',
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+});
